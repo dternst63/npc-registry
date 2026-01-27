e5422b6b01bdf3fa09742457e7119281b2ff093a
dternst63 <dternst63@gmail.com>
2026-01-27 14:21:45 -0600

    adjustcode in test file used in CI pipeline

--- client/ui-tests/npc-delete-flow.spec.ts
+++ client/ui-tests/npc-delete-flow.spec.ts
@@ -7,7 +7,7 @@ test("Delete NPC flow works", async ({ page }) => {
   let npcStore: any[] = [];
 
   // ---- API Mock Layer ----
-  await page.route("**/api/npcs", async (route) => {
+  await page.route("**/api/npcs*", async (route) => {
     const req = route.request();
     const method = req.method();
 
